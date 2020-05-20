79a568c1081d00c503245cff98db118bf4c269d3
Alex <sasha-elahov@yandex.ru>
2020-05-20 21:20:41 +0300

    add structure for listeners

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,10 +3,20 @@ import {ExcelComponent} from '../../core/ExcelComponents';
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['input'],
+    });
+  }
+
   toHTML() {
     return `
       <div class="icon">fx</div>
       <div class="input" contenteditable spellcheck="false"></div>
     `;
+  }
+
+  onInput() {
+    console.log('On inpu listener');
   }
 }

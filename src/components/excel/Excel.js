c566d929787cfa3d83aff7c80f419eaabf3e2a98
Alex <sasha-elahov@yandex.ru>
2020-05-23 18:24:12 +0300

    add excel table render

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -23,6 +23,5 @@ export class Excel {
   render() {
     this.$el.append(this.getRoot());
     this.components.forEach((component) => component.init());
-    this.components.forEach((component) => component.destroy());
   }
 }

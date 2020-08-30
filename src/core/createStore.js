7ba2f7c5721bb9fff9f08d13643d574500a38206
Alex <sasha-elahov@yandex.ru>
2020-08-30 18:50:34 +0300

    add createStore

---
+++ src/core/createStore.js
@@ -0,0 +1,20 @@
+export function createStore(rootReducer, initialState) {
+  let state = rootReducer({...initialState}, {type: '__INIT__'});
+  let listeners = [];
+
+  return {
+    subscribe(fn) {
+      listeners.push(fn);
+      return {
+        unsubscribe() {
+          listeners = listeners.filter((ls) => ls !== fn);
+        },
+      };
+    },
+    dispatch(action) {
+      state = rootReducer(state, action);
+      listeners.map((ls) => ls(state));
+    },
+    getState() {},
+  };
+}
